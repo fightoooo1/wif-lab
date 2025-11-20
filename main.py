10132bd3f98fd04eb079007def6765def9071433
fightoooo1 <nemamnik16@gmail.com>
2025-11-20 20:35:17 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "WIF Lab Success – Deployed via GitHub Actions with WIF!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
